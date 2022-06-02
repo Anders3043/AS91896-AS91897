116add63e0d68642af40c459bc74cc2729884439
Anders <st21184@ormiston.school.nz>
2022-06-02 12:07:05 +1200

    Started on buttons

--- AS91897.py
+++ AS91897.py
@@ -1,3 +1,14 @@
 # This program is for Julie to keep track of the item she hired out to people
 
 from tkinter import *
+
+def buttons():
+    global first_name, last_name, item, amount, recpit
+    first_name = Entry().grid(column=2, row=2)
+    Label(text="First name").grid(column=1, row=2)
+
+
+root = Tk()
+buttons()
+root.geometry("800x1000")
+root.mainloop()
