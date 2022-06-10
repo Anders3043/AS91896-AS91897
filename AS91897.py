83a926fe338ac16a7881de23a5b7af73ea567779
Anders <st21184@ormiston.school.nz>
2022-06-10 14:41:34 +1200

    made print button

--- AS91897.py
+++ AS91897.py
@@ -3,6 +3,7 @@
 from tkinter import *
 from tkinter import ttk
 
+total_entry = 0
 
 # Making entries
 def entries():
@@ -34,6 +35,8 @@ def quit_program():
 def buttons():
     Button(main_window, text="Quit", command=quit_program).grid(column=1, row=0)
     Button(main_window, text="Append details", command=append).grid(column=4, row=2)
+    Label().grid(column=4, row=3)
+    Button(main_window, text="Print details", command=print_list).grid(column=4, row=4)
 
 
 # setting up for error message
@@ -41,9 +44,14 @@ first_name_error = 0
 last_name_error = 0
 item_error = 0
 amount_error = 0
+# set up for append and prints
+hire_list = []
+count = 0
+
 
 def append():
-    global first_name, last_name, item, amount, receipt, first_name_error, first_name_label, last_name_error, last_name_label, item_error, item_label, amount_label, amount_error
+    global first_name, last_name, item, amount, receipt, first_name_error, first_name_label, last_name_error, \
+        last_name_label, item_error, item_label, amount_label, amount_error, total_entry, hire_list
     total_entries = 0
     try:
         int(amount.get())
@@ -107,6 +115,34 @@ def append():
     else:
         total_entries += 1
         amount_error = 0
+    if len(first_name.get()) != 0 and len(last_name.get()) != 0 and len(item.get()) != 0 and it_is == "True":
+        hire_list.append(first_name.get())
+        hire_list.append(last_name.get())
+        hire_list.append(item.get())
+        hire_list.append(amount.get())
+        first_name.delete(0, 'end')
+        last_name.delete(0, 'end')
+        item.delete(0, 'end')
+        amount.delete(0, 'end')
+        print(hire_list)
+        total_entry += 1
+
+
+# Making print function
+def print_list():
+    global total_entry, count
+    count = 0
+    Label(main_window, font='bold', text="First name").grid(column=1, row=10)
+    Label(main_window, font='bold', text="Last name").grid(column=2, row=10)
+    Label(main_window, font='bold', text="Item hired").grid(column=3, row=10)
+    Label(main_window, font='bold', text="Amount of item hired").grid(column=4, row=10)
+
+    if count < total_entry:
+        Label(main_window, text=(hire_list[0])).grid(column=1, row=count + 11)
+        Label(main_window, text=(hire_list[1])).grid(column=2, row=count + 11)
+        Label(main_window, text=(hire_list[2])).grid(column=3, row=count + 11)
+        Label(main_window, text=(hire_list[3])).grid(column=4, row=count + 11)
+        count += 1
 
 
 # Running the window
